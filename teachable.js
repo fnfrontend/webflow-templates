1f392ccc58f704c71d2a8ee8f390738d0aa8ff66
Farhad Nawab <Farhadnawab.official@gmail.com>
2019-11-24 16:32:38 +0500

    Create teachable.js

---
+++ teachable.js
@@ -0,0 +1,9 @@
+(function ($) {
+
+  $(document).ready(function(){
+      $("html").attr("data-wf-domain","click-funnels-treasure-chest.webflow.io");
+      $("html").attr("data-wf-page","5dd6ab56eab36e5bec7d4535"); 
+      $("html").attr("data-wf-site","5dd6ab56eab36e61da7d4515");
+      $("html").attr("data-wf-status","1");
+  });
+}(jQuery));
