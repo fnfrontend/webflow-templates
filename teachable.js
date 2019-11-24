e0f2105ad5936a1a49f21b919a3b38f23d5ca865
Farhad Nawab <Farhadnawab.official@gmail.com>
2019-11-24 16:49:47 +0500

    Update teachable.js

--- teachable.js
+++ teachable.js
@@ -5,5 +5,10 @@
       $("html").attr("data-wf-page","5dd6ab56eab36e5bec7d4535"); 
       $("html").attr("data-wf-site","5dd6ab56eab36e61da7d4515");
       $("html").attr("data-wf-status","1");
+    
+      setTimeout(function(){
+        $("html").append('<script src="https://uploads-ssl.webflow.com/5dd6ab56eab36e61da7d4515/js/webflow.11c8e3c9a.js" type="text/javascript"></script>');
+        alert("triggered1");
+      },500);
   });
 }(jQuery));
