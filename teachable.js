ae25e7e9a131cf1349001ae536046ef1f49d2f62
Farhad Nawab <Farhadnawab.official@gmail.com>
2019-11-24 17:53:41 +0500

    Update teachable.js

--- teachable.js
+++ teachable.js
@@ -1,15 +1,14 @@
 (function ($) {
 
-  $(document).ready(function(){
-      $("html").attr("data-wf-domain","click-funnels-treasure-chest.webflow.io");
-      $("html").attr("data-wf-page","5dd6ab56eab36e5bec7d4535"); 
-      $("html").attr("data-wf-site","5dd6ab56eab36e61da7d4515");
-      $("html").attr("data-wf-status","1");
-    
-      setTimeout(function(){
-        var newScript = document.createElement("script");
-        newScript.src = "https://uploads-ssl.webflow.com/5dd6ab56eab36e61da7d4515/js/webflow.11c8e3c9a.js";
-        document.getElementsByTagName("HTML")[0].appendChild(newScript);
-      },500);
-  });
+  var htmlSelec = document.getElementsByTagName("HTML")[0];
+  htmlSelec.setAttribute("data-wf-domain","click-funnels-treasure-chest.webflow.io");
+  htmlSelec.setAttribute("data-wf-page","5dd6ab56eab36e5bec7d4535"); 
+  htmlSelec.setAttribute("data-wf-site","5dd6ab56eab36e61da7d4515");
+  htmlSelec.setAttribute("data-wf-status","1");
+
+  setTimeout(function(){
+    var newScript = document.createElement("script");
+    newScript.src = "https://uploads-ssl.webflow.com/5dd6ab56eab36e61da7d4515/js/webflow.11c8e3c9a.js";
+    document.getElementsByTagName("HTML")[0].appendChild(newScript);
+  },500);
 }(jQuery));
